e05085aaaed8b38ee717e23aba27144dcaf32ac7
Andreas Simek <andreas.simek@3kraft.com>
2019-02-16 13:41:52 +0100

    Options added to close a popover on click

--- src/bindings/popoverBinding.js
+++ src/bindings/popoverBinding.js
@@ -71,6 +71,31 @@ ko.bindingHandlers.popover = {
         if (!popoverData) {
             $element.popover(options);
 
+            // Check options whether the popover should be closed on click automatically.
+            // - closeOnClickOutside -> clicks inside the popover are ignored.
+            // - closeOnClick -> all clicks will close the popover
+            var autoClose = (options.closeOnClick && options.closeOnClick === true)
+                || (options.closeOnClickOutside && options.closeOnClickOutside === true);
+            if(autoClose) {
+                // bind a custom event when the popover is shown.
+                $element.on('show.bs.popover', function () {
+                    $(this).addClass('popover-trigger');
+                    $('body').on("click.closePopover", function () {
+                        var clickOnPopover = options.closeOnClick ? false : $(event.target).closest(".popover.show").length > 0;
+                        var clickOnPopoverTrigger = $(event.target).closest(".popover-trigger").length > 0;
+                        if (!clickOnPopover && !clickOnPopoverTrigger) {
+                            $element.popover('hide');
+                        }
+                    });
+                });
+
+                // remove the custom event when the popover is hidden.
+                $element.on('hide.bs.popover', function () {
+                    $('body').off("click.closePopover");
+                    $(this).removeClass('popover-trigger');
+                });
+            }
+
             $element.on('shown.bs.popover inserted.bs.popover', function () {
                 (options.container ? $(options.container) : $element.parent()).one('click', '[data-dismiss="popover"]', function () {
                     $element.popover('hide');
