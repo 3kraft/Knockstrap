cd795bc3c5a7ed582a4996972583da70d628e92c
Andreas Simek <andreas.simek@3kraft.com>
2019-09-24 19:48:32 +0200

    Show first tab with knockout validation message

--- src/bindings/tabs.js
+++ src/bindings/tabs.js
@@ -25,5 +25,25 @@ ko.bindingHandlers.tabs = {
                 .on('hide.bs.tab', options.onHide)
                 .on('hidden.bs.tab', options.onHidden);
         });
+    },
+    update: function (element, valueAccessor) {
+
+        var options = $.extend(true, {}, ko.bindingHandlers.tabs.defaults, valueAccessor());
+
+        // show the first tab pane with an error.
+        $(element).find(options.tabSelector).each(function() {
+            var $tab = $(this);
+            var paneId = $tab.attr('href');
+            if (typeof paneId !== typeof undefined && paneId !== false) {
+                var $pane = $(paneId);
+                var $errors = $pane.find(".validationMessage").filter(function( index, element ) {
+                    return $(element).text().length > 0;
+                });
+                if($errors.length > 0) {
+                    $tab.tab('show');
+                    return false;
+                }
+            }
+        });
     }
 };
